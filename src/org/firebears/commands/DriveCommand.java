acd7668428737bd301ceb4916b71a44967de142c
Tyler <Tylerhockeyguy11@gmail.com>
2017-02-12 15:36:25 -0600

    Fixed Joystick Z sticky bug

--- src/org/firebears/commands/DriveCommand.java
+++ src/org/firebears/commands/DriveCommand.java
@@ -11,8 +11,10 @@
 
 package org.firebears.commands;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import org.firebears.Robot;
-import org.firebears.RobotMap;
+
 
 /**
  *
@@ -41,88 +43,91 @@ public class DriveCommand extends Command {
     }
     
     
-//    double strafe3 = 0.0;
-//    double forward3 = 0.0;
-//    double rotation3 = 0.0;
-//    double strafe4 = 0.0;
-//    double forward4 = 0.0;
-//    double rotation4 = 0.0;
-//    double oy = 0.0;
-//    double ox = 0.0;
-//    double oz = 0.0;
+    double strafe3 = 0.0;
+    double forward3 = 0.0;
+    double rotation3 = 0.0;
+    double strafe4 = 0.0;
+    double forward4 = 0.0;
+    double rotation4 = 0.0;
+    double oy = 0.0;
+    double ox = 0.0;
+    double oz = 0.0;
 
     // Called repeatedly when this Command is scheduled to run
     protected void execute() {
 
-//    	
-//    	//Drive with one Joystick
-//    	double strafe = Robot.oi.joystick1.getRawAxis(0);
-//    	double forward = Robot.oi.joystick1.getRawAxis(1);
-//    	double rotation = Robot.oi.joystick1.getRawAxis(2);
-//    	
-//    	//Drive with two Joysticks
-////    	double forward = Robot.oi.joystick1.getY();
-////    	double strafe = Robot.oi.joystick1.getX(); 
-////    	double rotation = Robot.oi.joystick2.getX();
-//    	
-//    	
-//    	//set to minimum joystick value were the robot moves(Only do this if you do NOT have PID Driving!AKA lEAVE AT ZERO WHEN PID)
-//    	double robotdedy = 0.00;
-//    	double robotdedx = 0.00;
-//    	double robotdedz = 0.00;
-//    	
-//    	//Deadzone of the joysticks
-//    	double Joydedy = 0.1;
-//    	double Joydedx = 0.04;
-//    	double Joydedz = 0.07;
-//    	
-//    	//Add a powercurve (((senativaty in the lower and upper values)))
+    	//Drive with one Joystick
+    	double strafe = Robot.oi.joystick1.getRawAxis(0);
+    	double forward = Robot.oi.joystick1.getRawAxis(1);
+    	double rotation = Robot.oi.joystick1.getRawAxis(2);
+    	
+//    	double forward = Robot.oi.joystick1.getY();
+//    	double strafe = Robot.oi.joystick1.getX(); 
+//    	double rotation = Robot.oi.joystick1.getZ();
+    	
+    	//Drive with two Joysticks
+//    	double forward = Robot.oi.joystick1.getY();
+//    	double strafe = Robot.oi.joystick1.getX(); 
+//    	double rotation = Robot.oi.joystick2.getX();
+    	
+    	
+    	//set to minimum joystick value were the robot moves(Only do this if you do NOT have PID Driving!AKA lEAVE AT ZERO WHEN PID)
+    	double robotdedy = 0.00;
+    	double robotdedx = 0.00;
+    	double robotdedz = 0.00;
+    	
+    	//Deadzone of the joysticks
+    	double Joydedy = 0.11;
+    	double Joydedx = 0.04;
+    	double Joydedz = 0.07;
+    	
+    	//Add a powercurve (((senativaty in the lower and upper values)))
 //    	double sensxy = 1.1;
 //    	double sensez = 1.15;
-//    	
-////    	SmartDashboard.putNumber("JoyY", forward);
-////    	SmartDashboard.putNumber("JoyX", strafe);
-////    	SmartDashboard.putNumber("JoyZ", rotation);
-//
-//    	double y2 = forward * (1 - robotdedy)/1;
-//    	double x2 = strafe * (1 - robotdedx)/1;
-//    	double z2 = rotation * (1 - robotdedz)/1;
-//    		
-//    	
-//    	//Foward Deadzone
-//    	if (forward > Joydedy){
-//    		forward3 = (y2 + robotdedy - Joydedy) * ((1 + Joydedy)/1);
-//    	}
-//    	
-//    	else if (forward < -Joydedy){
-//    		forward3 = (y2 - robotdedy + Joydedy) * ((1 + Joydedy)/1);
-//    	}
-//    	else if (forward < Joydedy && forward > -Joydedy)
-//    		forward3 = 0.0;
-//    	
-//    	//Strafe Deadzone
-//    	if (strafe > Joydedy){
-//    		strafe3 = (x2 + robotdedx - Joydedx) * ((1 + Joydedx)/1);
-//    	}
-//    	
-//    	else if (strafe < -Joydedx){
-//    		strafe3 = (x2 - robotdedx + Joydedx) * ((1 + Joydedx)/1);
-//    	}
-//    	else if (strafe < Joydedx && strafe > -Joydedx)
-//    		strafe3 = 0.0;
-//    	//Rotation Deadzone
-//    	if (rotation > Joydedz){
-//    		rotation3 = (z2 + robotdedz - Joydedz) * ((1 + Joydedz)/1);
-//    	}
-//    	
-//    	else if (rotation < -Joydedz){
-//    		rotation3 = (z2 - robotdedz + Joydedz) * ((1 + Joydedz)/1);
-//    	}
-//    	else if (rotation < Joydedz && rotation > - Joydedz)
-//    		rotation3 = 0.0;
-//    	
-//    	//strafe sense
-//    	
+    	
+//    	SmartDashboard.putNumber("JoyY", forward);
+//    	SmartDashboard.putNumber("JoyX", strafe);
+//    	SmartDashboard.putNumber("JoyZ", rotation);
+
+    	double y2 = forward * (1 - robotdedy)/1;
+    	double x2 = strafe * (1 - robotdedx)/1;
+    	double z2 = rotation * (1 - robotdedz)/1;
+    		
+    	
+    	//Foward Deadzone
+    	if (forward > Joydedy){
+    		forward3 = (y2 + robotdedy - Joydedy) * ((1 + Joydedy)/1);
+    	}
+    	
+    	else if (forward < -Joydedy){
+    		forward3 = (y2 - robotdedy + Joydedy) * ((1 + Joydedy)/1);
+    	}
+    	else if (forward < Joydedy && forward > -Joydedy)
+    		forward3 = 0.0;
+    	
+    	//Strafe Deadzone
+    	if (strafe > Joydedy){
+    		strafe3 = (x2 + robotdedx - Joydedx) * ((1 + Joydedx)/1);
+    	}
+    	
+    	else if (strafe < -Joydedx){
+    		strafe3 = (x2 - robotdedx + Joydedx) * ((1 + Joydedx)/1);
+    	}
+    	else if (strafe < Joydedx && strafe > -Joydedx)
+    		strafe3 = 0.0;
+    	//Rotation Deadzone
+    	if (rotation > Joydedz){
+    		rotation3 = (z2 + robotdedz - Joydedz) * ((1 + Joydedz)/1);
+    	}
+    	
+    	else if (rotation < -Joydedz){
+    		rotation3 = (z2 - robotdedz + Joydedz) * ((1 + Joydedz)/1);
+    	}
+    	else if (rotation < Joydedz && rotation > - Joydedz)
+    		rotation3 = 0.0;
+    	
+    	//strafe sense
+    	
 //    	if (strafe < 0.0){
 //    		double ox = strafe3 * -1;
 //    		strafe4 = -Math.pow(ox, sensxy);
@@ -151,10 +156,16 @@ public class DriveCommand extends Command {
 //    	else if (rotation > 0.0){
 //    		rotation4 = Math.pow(rotation3, sensez);
 //    	}
-//    	
-//    	
     	
-    	Robot.chassis.drive(RobotMap.joysticksettings.strafe4, RobotMap.joysticksettings.forward4, RobotMap.joysticksettings.rotation4);
+//    	SmartDashboard.putNumber("Rotation4", rotation3);
+//    	SmartDashboard.putNumber("JoyZ", rotation);
+//    	SmartDashboard.putNumber("Strafe4", strafe3);
+//    	SmartDashboard.putNumber("JoyX", strafe);
+//    	SmartDashboard.putNumber("Forward4", forward3);
+//    	SmartDashboard.putNumber("JoyY", forward);
+
+    	
+    	Robot.chassis.drive(strafe3, forward3, rotation3);
     	
     }
 
